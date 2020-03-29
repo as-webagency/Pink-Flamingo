875f15fbbb061235005bbf6d53f64bb38d7cb507
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-03-29 12:37:44 +0300

    add header, hero and servies.sass files

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
     
-    //
+    // Меню
+    const toggleMenu = () => {
+
+        const menu = document.querySelector('.nav__block'),
+            body = document.querySelector('body'),
+            maskNav = document.querySelector('.mask-nav');
+
+        body.addEventListener('click', (event) => {
+
+            let target = event.target;
+
+            if (target.closest('.burger')) {
+                menu.classList.add('nav__block-active');
+                maskNav.style.display = 'block';
+            } else if (target.classList.contains('nav__close') || 
+                target.classList.contains('mask-nav')) {
+                menu.classList.remove('nav__block-active');
+                maskNav.style.display = 'none';
+            } else if (target.tagName !== 'DIV') {
+                menu.classList.remove('nav__block-active');
+                maskNav.style.display = 'none';
+            } else {
+                return;
+            }
+
+        });
+
+    };
+
+    toggleMenu();
 
 });
