ce676941ed3f48dc5fd625555f13bc197baea329
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-04-05 18:56:47 +0300

    add forms

--- src/send.php
+++ src/send.php
@@ -2,8 +2,8 @@
 
 $userName = $_POST['user_name'];
 $userPhone = $_POST['user_phone'];
-$userEmail = $_POST['user_email'];
-$userQuestion = $_POST['user_question'];
+$userCall = $_POST['user_call'];
+$userQuestions = $_POST['user_text'];
 
 
 // Load Composer's autoloader
@@ -22,22 +22,22 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'semenovaleksandr407@gmail.com';                     // SMTP username
-    $mail->Password   = '242175rko';                               // SMTP password
+    $mail->Password   = '242175rko05';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('semenovaleksandr407@gmail.com', 'Repair Design Project');
+    $mail->setFrom('semenovaleksandr407@gmail.com', 'Розовый Фламинго');
     $mail->addAddress('sniper.semenov@ukr.net', 'Joe User');     // Add a recipient
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Новая заявка с сайта - "Турецкий ремонт в Ростове-на-Дону"';
+    $mail->Subject = 'Новая заявка с сайта - "Розовый Фламинго"';
     $mail->Body    = '
-        Имя: ' . $userName . '<br> 
-        Телефон: ' . $userPhone . '<br> 
-        Email: ' . $userEmail . '<br> 
-        Вопрос: ' . $userQuestion . '
+        Как к Вам обращаться: ' . $userName . '<br> 
+        Телефон: ' . $userPhone . '<br>
+        Способ связи: ' . $userCall . '<br>
+        Ваш вопрос: ' . $userQuestions . '
     ';
 
     if ($mail->send()) {
